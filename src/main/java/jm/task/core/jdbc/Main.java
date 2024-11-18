e4dea0873fa57fd6833f4d74350f07dade154528
Denis Akhmetov <jira.gremory@gmail.com>
2024-11-19 00:06:47 +0300

    Реализованы необходимые методы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        System.out.println("Создается таблица...");
+        userDao.createUsersTable();
+        System.out.println("Таблица создана, метод отработал успешно.");
+
+        userDao.saveUser("Alex", "Nolan", (byte) 34);
+        userDao.saveUser("July", "Bundera", (byte) 29);
+        userDao.saveUser("Anna", "Smirnova", (byte) 15);
+        userDao.saveUser("Andrey", "Shaman", (byte) 45);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers().forEach(System.out::println);
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/jdbc_hibernate";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Config$MySQL!1";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            System.err.println("Ошибка подключения к базе данных: " + e.getMessage());
+            throw new RuntimeException("Не удается подключиться к базе данных", e);
+        }
+        return connection;
+    }
 }
